570c2b23996411d7f426146ee0aa63693a306e2c
Dale Smith <desmith100@gmail.com>
2020-04-30 12:06:30 -0400

    Initial directory structure

---
+++ poses/src/Controllers/PosesController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using src.Models;
+
+namespace poses.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PosesController : ControllerBase
+    {
+        private static readonly List<Pose> Poses = new List<Pose>
+        {
+            new Pose 
+            {
+                Name = "Warrior 1",
+                Sanskrit = "Virabhadrasana I",
+                Level = "Beginner",
+                Tags = new List<string>{ "standing" }
+            },
+            new Pose 
+            {
+                Name = "Warrior 2",
+                Sanskrit = "Virabhadrasana II",
+                Level = "Beginner",
+                Tags = new List<string>{ "standing" }
+            },
+            new Pose 
+            {
+                Name = "Warrior 3",
+                Sanskrit = "Virabhadrasana III",
+                Level = "Beginner",
+                Tags = new List<string>{ "standing", "one-legged" }
+            },
+        };
+
+        private readonly ILogger<PosesController> _logger;
+
+        public PosesController(ILogger<PosesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Poses);
+        }
+    }
+}
---
+++ poses/src/Models/Pose.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace src.Models
+{
+    public class Pose
+    {
+        public string Name { get; set; }
+
+        public string Sanskrit { get; set;} 
+
+        public string Level { get; set; }
+
+        public List<string> Tags { get; set; }
+    }
+}
